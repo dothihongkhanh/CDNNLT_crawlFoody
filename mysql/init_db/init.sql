b1c0d4c2b023e889de3a7eb079cd2e3261efd045
dothihongkhanh <dothihongkhanh22@gmail.com>
2023-04-21 00:43:42 +0700

    insert data

--- mysql/init_db/init.sql
+++ mysql/init_db/init.sql
@@ -17,10 +17,10 @@ USE foody_db;
 CREATE TABLE Dishes (
 dish_id varchar(15) NOT NULL PRIMARY KEY,
 dish_name nvarchar(200),
-price int ,
+price varchar(200) ,
 d_description nvarchar(200),
 dish_type_name nvarchar(200),
-delivery_id nvarchar(200)
+delivery_id varchar(200)
 );
 
 
--- mysql_api/main.py
+++ mysql_api/main.py
@@ -1,11 +1,41 @@
+import pandas as pd
 import mysql.connector
-cnx = mysql.connector.connect(user='root', password='psw123', port='6603', host='127.0.0.1', database='foody_db')
-cursor = cnx.cursor()
-table_name = 'Delivery_Infos'
-add_delivery_info = (f"INSERT INTO {table_name} " "(delivery_id, delivery_name, phones , delivery_address, rating ) "   "VALUES (%s, %s, %s, %s, %s)")
-data_delivery_info = ('222', '300','ssssssssssssssssssss','Conan','5')
+import csv
 
-cursor.execute(add_delivery_info, data_delivery_info)
-cnx.commit() # lưu những dữ liệu chúng ta đã chèn vào DB
-cursor.close()
-cnx.close()
+# Kết nối đến cơ sở dữ liệu
+mydb = mysql.connector.connect(user='root', password='psw123', port='6603',
+host='127.0.0.1',
+database='foody_db')
+
+# Tạo một con trỏ để thao tác với database
+mycursor = mydb.cursor()
+
+# Đọc file CSV
+
+with open('../data_crawl/code/dishes.csv', 'r', encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile)
+    next(csvreader) # bỏ qua dòng tiêu đề
+    for row in csvreader:
+        # xử lý dữ liệu trong mỗi row
+        # ví dụ, lưu dữ liệu vào database
+        sql = "INSERT INTO Dishes (dish_id, dish_name, price,d_description,dish_type_name,delivery_id) VALUES (%s, %s, %s, %s, %s, %s)"
+        val = (row[0], row[1], row[2],row[3],row[4],row[5])
+        mycursor.execute(sql, val)
+
+mydb.commit()
+
+# Đóng kết nối đến cơ sở dữ liệu
+mycursor.close()
+mydb.close()
+
+
+# cnx = mysql.connector.connect(user='root', password='psw123', port='6603', host='127.0.0.1', database='foody_db')
+# cursor = cnx.cursor()
+# table_name = 'Delivery_Infos'
+# add_delivery_info = (f"INSERT INTO {table_name} " "(delivery_id, delivery_name, phones , delivery_address, rating ) "   "VALUES (%s, %s, %s, %s, %s)")
+# data_delivery_info = ('222', '300','ssssssssssssssssssss','Conan','5')
+
+# cursor.execute(add_delivery_info, data_delivery_info)
+# cnx.commit() # lưu những dữ liệu chúng ta đã chèn vào DB
+# cursor.close()
+# cnx.close()
